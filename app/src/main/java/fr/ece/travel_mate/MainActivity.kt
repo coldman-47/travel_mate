84356cd1407620be631f2a5e64ba6099695157c1
Omar <ocamara015@gmail.com>
2023-11-15 12:20:26 +0100

    1st commit

---
+++ app/src/main/java/fr/ece/travel_mate/MainActivity.kt
@@ -0,0 +1,77 @@
+package fr.ece.travel_mate
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import fr.ece.travel_mate.databinding.ActivityMainBinding
+import fr.ece.travel_mate.databinding.HomeBinding
+import fr.ece.travel_mate.databinding.LoginBinding
+import fr.ece.travel_mate.databinding.RegisterBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: LoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var btn = findViewById<Button>(R.id.loginBtn)
+        var registerBtn = findViewById<Button>(R.id.registerLink)
+
+        btn.setOnClickListener(View.OnClickListener {
+            setContentView(HomeBinding.inflate(layoutInflater).root)
+        })
+        registerBtn.setOnClickListener(View.OnClickListener {
+            setContentView(RegisterBinding.inflate(layoutInflater).root)
+        })
+
+        /*
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        */
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}

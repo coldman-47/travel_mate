91b6e6b7a3dab4f05352beed75a1051e5c3fdca6
Omar <ocamara015@gmail.com>
2023-12-12 01:01:00 +0100

    logout

--- app/src/main/java/fr/ece/travel_mate/MainActivity.kt
+++ app/src/main/java/fr/ece/travel_mate/MainActivity.kt
@@ -1,11 +1,14 @@
 package fr.ece.travel_mate
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -27,67 +30,55 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-        val btn = findViewById<Button>(R.id.loginBtn)
-        val registerBtn = findViewById<Button>(R.id.registerLink)
-        btn.setOnClickListener(View.OnClickListener {
-            val login = findViewById<AppCompatEditText>(R.id.loginEmail).text
-            val password = findViewById<EditText>(R.id.loginPwd).text
-            auth.signInWithEmailAndPassword(login.toString(), password.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
+        val sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
+        Log.d(TAG, "boom "+sharedPreferences.all["user"].toString())
+        if (sharedPreferences.all["user"] != null) {
+            loadHomePage()
+        } else {
+            setContentView(binding.root)
+            // Initialize Firebase Auth
+            auth = Firebase.auth
+            val btn = findViewById<Button>(R.id.loginBtn)
+            val registerBtn = findViewById<Button>(R.id.registerLink)
+            btn.setOnClickListener(View.OnClickListener {
+                val login = findViewById<AppCompatEditText>(R.id.loginEmail).text
+                val password = findViewById<EditText>(R.id.loginPwd).text
+                auth.signInWithEmailAndPassword(login.toString(), password.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            sharedPreferences.edit()
+                                .putString("user", task.result.user?.email.toString()).commit()
 //                        setContentView(HomeBinding.inflate(layoutInflater).root)
-                        val intent = Intent(this@MainActivity, HomeActivity::class.java)
-                        startActivity(intent)
+                            loadHomePage()
 
-                    } else {
-                        Toast.makeText(
-                            baseContext,
-                            "Connexion refusée.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
+                        } else {
+                            Toast.makeText(
+                                baseContext,
+                                "Connexion refusée.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
                     }
-                }
-        })
-        registerBtn.setOnClickListener(View.OnClickListener {
-            setContentView(RegisterBinding.inflate(layoutInflater).root)
-            val loginLink: Button = findViewById(R.id.loginLink)
-            loginLink.setOnClickListener(View.OnClickListener {
-                setContentView(binding.root)
             })
-        })
-
-        /*
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            registerBtn.setOnClickListener(View.OnClickListener {
+                setContentView(RegisterBinding.inflate(layoutInflater).root)
+                val loginLink: Button = findViewById(R.id.loginLink)
+                loginLink.setOnClickListener(View.OnClickListener {
+                    setContentView(binding.root)
+                })
+            })
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        */
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    private fun loadHomePage() {
+        val intent = Intent(this@MainActivity, HomeActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onSupportNavigateUp(): Boolean {
